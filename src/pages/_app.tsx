4e10f33e65aa0a2b465b2391144d395a0dbd0f5a
nakajimaibuki <nakajimai@crunchtimer.jp>
2020-11-30 21:01:54 +0900

    update: fix eslint error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/index.css"
 import React from "react"
+import PropTypes from "prop-types"
 
-const MyApp: React.FC<any> = ({ Component, pageProps }) => {
+const MyApp: React.FC<{ Component: any; pageProps: any }> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
+}
+MyApp.propTypes = {
+  Component: PropTypes.func,
+  pageProps: PropTypes.object
 }
 
 export default MyApp
