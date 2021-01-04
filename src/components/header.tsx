169e5e8ccce7c5edb630bbff6e45f9925cd087e0
hourglasshoro <hourglasshoro.628@gmail.com>
2021-01-04 13:55:33 +0900

    update: Delete menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,27 +12,28 @@ const Header: React.FC<{}> = () => {
           </div>
         </div>
         <div className="flex justify-end items-center py-6 md:space-x-10 px-4 sm:px-6">
-          <button
-            type="button"
-            className="rounded-md p-2 inline-flex items-center justify-center text-white hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-          >
-            <span className="sr-only">Open menu</span>
-            <svg
-              className="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
+          <div className="h-6" />
+          {/*<button*/}
+          {/*  type="button"*/}
+          {/*  className="rounded-md p-2 inline-flex items-center justify-center text-white hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"*/}
+          {/*>*/}
+          {/*  <span className="sr-only">Open menu</span>*/}
+          {/*  <svg*/}
+          {/*    className="h-6 w-6"*/}
+          {/*    xmlns="http://www.w3.org/2000/svg"*/}
+          {/*    fill="none"*/}
+          {/*    viewBox="0 0 24 24"*/}
+          {/*    stroke="currentColor"*/}
+          {/*    aria-hidden="true"*/}
+          {/*  >*/}
+          {/*    <path*/}
+          {/*      strokeLinecap="round"*/}
+          {/*      strokeLinejoin="round"*/}
+          {/*      strokeWidth="2"*/}
+          {/*      d="M4 6h16M4 12h16M4 18h16"*/}
+          {/*    />*/}
+          {/*  </svg>*/}
+          {/*</button>*/}
         </div>
       </div>
 
